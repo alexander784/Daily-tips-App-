465ce960ae1cc7059b36d07f333874453eaf4cda
alexander <alexandernyaga1999@gmail.com>
2023-10-12 06:13:14 +0300

    initialize currnet tip store tips and backgroundurl

---
+++ script.js
@@ -0,0 +1,6 @@
+//initialize index for current tip
+let currentTipIndex = 0;
+//create an empty array to store tips
+let tips = [];
+//initialize an empty string for backgroundURL image
+let backgroundImageUrl = ""
