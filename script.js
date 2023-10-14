dc1bc1a23e63a66c395c582872227c6046e05fca
alexander <alexandernyaga1999@gmail.com>
2023-10-14 13:33:05 +0300

    display tips function

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
 //reference from html
 const tipText = document.getElementById("tip-text");
+console.log(tipText.textContent);
 const nextButton = document.getElementById("next-button");
-
 
 //initialize index for current tip
 let currentTipIndex = 0;
 //create an empty array to store tips
-let tips = [];
+let tipsArray = [];
 //initialize an empty string for backgroundURL image
 let backgroundImageUrl = ""
 
 //function to display tips
-function displayTips(index) {
-    const tip = tips[index];
-    if(tip) {
-        tipText.textContent = tip.text;
-        document.body.style.backgroundImage = `url(${tip.backgroundImage})`;
-        
-    }else{
-        tipText.textContent = "No more tips available.";
-
-    }
-    
+function displayTips() {
+    tipText.innerHTML = `
+    <img src="${tipsArray[currentTipIndex].backgroundImage}"  alt="" height = 200px width = 200px> <br>
+    ${tipsArray[currentTipIndex].text}
+    `;
+    currentTipIndex = (currentTipIndex +1)% tipsArray.length;
 }
 
 //add eventLister to  next button
 nextButton.addEventListener("click", () => {
-    currentTipIndex++;
-    if(currentTipIndex >= tips.length) {
-        currentTipIndex =0;
-    }
-    displayTips(currentTipIndex);
-});
-
-//fetch data from Json
-fetch("db.json")
+    fetch("http://localhost:3000/tips")
       .then((response) => response.json())
       .then((data) => {
-        tips = data.tips;
-        displayTip(currentTipIndex);
+        tipsArray = data;
+        console.log(tipsArray);
+
+        displayTips();
+
       })
       .catch((error) => {
         console.error("Error fetching tips:", error);
       });
+
+});
